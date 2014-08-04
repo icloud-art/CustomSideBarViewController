f6a81a1b9c79e24ef06b775a26ebffd6e6f3934c
Charles Leo <liuyahuino1@sina.com>
2014-08-04 18:06:59 +0800

    实现table数据展示

--- LYHSideBarCtrl/LeftViewController.m
+++ LYHSideBarCtrl/LeftViewController.m
@@ -8,8 +8,14 @@
 
 #import "LeftViewController.h"
 #import "MainViewController.h"
+#import "LYHTableDelegate.h"
+#import "LYHArrayDataSource.h"
+#import "LYHDataCell.h"
 @interface LeftViewController ()
-
+{
+    LYHArrayDataSource * _dataSource;
+    LYHTableDelegate * _delegate;
+}
 @end
 
 @implementation LeftViewController
@@ -32,16 +38,27 @@
     }
     UITableView * tableView = [[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStylePlain];
     [self.view addSubview:tableView];
+    TableViewCellConfigureBlock configureBlock = ^(LYHDataCell *cell,NSString * title){
+        [cell configureForData:title];
+    };
+    NSArray * array = @[@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10"];
+    _dataSource = [[LYHArrayDataSource alloc]initWithItems:array cellItentifier:@"ID" configureCellBlock:configureBlock];
+    tableView.dataSource = _dataSource;
+    
+    TableViewDelegateConfigureBlock delegateBlock = ^(LYHDataCell *cell,NSIndexPath * indexPath){
+        
+    };
+    _delegate = [[LYHTableDelegate alloc]initWithCount:array.count andHeight:60 andConfigureBlock:delegateBlock];
+    tableView.delegate = _delegate;
+    [self.view addSubview:tableView];
 }
 - (UINavigationController *)subControllerWithIndex:(NSInteger)index
 {
     MainViewController * mainView =[[MainViewController alloc]init];
     mainView.title = @"自定义SideBar";
     UINavigationController * navCtrl = [[UINavigationController alloc]initWithRootViewController:mainView];
-  //  navCtrl.navigationBar.hidden = YES;
     return navCtrl;
 }
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
